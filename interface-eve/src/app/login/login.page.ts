215be56f285936e4ae066e9a1319b2bede36a91f
Allen Williams <allen.williams2@rockets.utoledo.edu>
2020-04-25 19:37:13 -0400

    Login connected to API

--- interface-eve/src/app/login/login.page.ts
+++ interface-eve/src/app/login/login.page.ts
@@ -8,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class LoginPage implements OnInit {
 
   constructor() { }
+  _user = ''; _password = '';
+  verifyUser() {
+    let xhr = new XMLHttpRequest();
+    let url = 'http://localhost:8000/verifyuser';
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState == 4) {
+        let raw_data = xhr.response;
+        let data = JSON.parse(raw_data);
+        console.log(data);
+      }
+    }
+    xhr.send(JSON.stringify({user: this._user, password: this._password}));
+  }
+
+  onUpdateUser(event: Event) {
+    this._user = <HTMLInputElement>event.target.value;
+  }
+  onUpdatePassword(event: Event) {
+    this._password = <HTMLInputElement>event.target.value;
+  }
 
   ngOnInit() {
   }
