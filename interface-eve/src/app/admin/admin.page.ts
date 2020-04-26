8c12db6ddc213353953ebee1962607d42dfc1586
Allen Williams <allen.williams2@rockets.utoledo.edu>
2020-04-26 18:47:50 -0400

    Add user on admin page works

--- interface-eve/src/app/admin/admin.page.ts
+++ interface-eve/src/app/admin/admin.page.ts
@@ -23,4 +23,24 @@ export class AdminPage implements OnInit {
   onUpdatePrivateKey(event: Event) {
     this._private_key = <HTMLInputElement>event.target.value;
   }
+  addUser() {
+    const that = this;
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:8000/newuser');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    let data = {};
+    data.user = that._user;
+    data.password = that._password;
+    data.address = that._address;
+    data.private_key = that._private_key;
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState == 4) {
+        console.log(xhr.response);
+      }
+    }
+    console.log(data);
+    console.log(JSON.stringify(data));
+    console.log(typeof JSON.stringify(data))
+    xhr.send(JSON.stringify(data));
+  }
 }
