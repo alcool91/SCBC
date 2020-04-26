b8977e593576d7e2bbf719c783698f3423270e91
Allen Williams <allen.williams2@rockets.utoledo.edu>
2020-04-26 12:48:40 -0400

    first admin page (add user)

--- interface-eve/src/app/admin/admin.page.ts
+++ interface-eve/src/app/admin/admin.page.ts
@@ -8,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class AdminPage implements OnInit {
 
   constructor() { }
-
+  _user=''; _password=''; _address=''; _private_key='';
   ngOnInit() {
   }
-
+  onUpdateUser(event: Event) {
+    this._user = <HTMLInputElement>event.target.value;
+  }
+  onUpdatePassword(event: Event) {
+    this._password = <HTMLInputElement>event.target.value;
+  }
+  onUpdateAddress(event: Event) {
+    this._address = <HTMLInputElement>event.target.value;
+  }
+  onUpdatePrivateKey(event: Event) {
+    this._private_key = <HTMLInputElement>event.target.value;
+  }
 }
